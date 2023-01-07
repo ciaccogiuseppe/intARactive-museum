dfe5cf58da2e06d6a84620a1916cdc4b71e96a14
Giacomo Bruno <s301311@studenti.polito.it>
2023-01-07 15:36:16 +0100

    Added questions for The Great Wave

--- Components/Quiz/QuestionsAndAnswers.js
+++ Components/Quiz/QuestionsAndAnswers.js
@@ -34,6 +34,41 @@ export const questionsSunflowers = [
   }
 ];
 
+export const questionsGreatWave = [
+  {
+    question: "When was \"The Great Wave off Kanagawa\" created?",
+    options: [
+      "In 1831",
+      "In 1931",
+      "In 1856",
+      "In 1801"
+    ],
+    solution: 0,
+    explanation: "\"The Great Wave off Kanagawa\" is a woodblock print by Japanese artist Hokusai, created in late 1831 during the Edo period of Japanese History."
+  },
+  {
+    question: "Which series does this print belong to?",
+    options: [
+      "One Hundred Views of Mount Fuji",
+      "Waves",
+      "Thirty-six Views of Mount Fuji",
+      "Blue Period"
+    ],
+    solution: 2,
+    explanation: "The print is Hokusai's best-known work and the first in his series \"Thirty-six-Views of Mount Fuji\", in which the use of Prussian blue revolutionized Japanese prints."
+  },
+  {
+    question: "Which art movement did the print inspired?",
+    options: [
+      "Expressionism",
+      "Realism",
+      "Romanticism",
+      "Impressionism"
+    ],
+    solution: 3,
+    explanation: "The composition of \"The Great Wave\" earned to the artist immediate success in Japan and later in Europe, where Hokusai's art inspired works by the Impressionist."
+  }
+];
 
 export var givenAnswersSunflowers = [];
     // when the user complete a quiz it will contain objects like this:
@@ -42,6 +77,8 @@ export var givenAnswersSunflowers = [];
     //   date: 30/12/2022 10:45
     //   score: 3
     //}
+    
+export var givenAnswersGreatWave = [];
 
 export let quizAnswered = [];
     // when the user complete a quiz it will contain objects like this:
