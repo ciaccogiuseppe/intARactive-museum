03cdc0734b8a42591f6c675418619824596bb1b5
unknown <s303398@studenti.polito.it>
2023-01-08 19:23:43 +0100

    ActivityBar v1.0

--- Globals/Components.js
+++ Globals/Components.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text, Icon } from "@rneui/themed";
+import { Text } from "@rneui/themed";
 import { TouchableHighlight } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 import styles from "./Styles";
 
-/*
-Props:
+/********************** PROPS ********************** 
 - titleName: title of the header
 - navigation: component for navigation between pages
-- onClose: function to call when user clicks on "x"
-*/
+- onClose: function to call when user clicks on "x". if the prop is undefined it shows the "?" icon
+- isHome: 
+    - true -> it shows an home icon
+    - false -> it shows a "<-" icon
+    - undefined: no left icons (a hidden one to maintain alignment)
+***************************************************/
+
 function ActivityBar(props) {
-    return <View style={{ ...styles.secondHeader }}>
-        <TouchableHighlight style={{ ...styles.button, width: "20%", alignSelf: 'center' }} onPress={() => props.navigation.navigate('Home')}>
-            <Text style={{ color: 'white', alignSelf: 'center' }}>Home</Text>
-        </TouchableHighlight>
-        <Text style={{ color: 'white', alignSelf: 'center', fontSize: 30 }}>
-            {props.titleName}
-        </Text>
-        {props.onClose != undefined ? <Icon name='close' type='material' onPress={props.onClose} color={'white'} alignSelf={"flex-end"}></Icon> : <></>}
-    </View>
+    return (
+        <View style={{
+            flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingVertical: 5,
+            backgroundColor: "#417D7AAA" /* The last two hex digits are the alpha channel of the color */
+        }}>
+            {props.isHome === true ? <Icon name='home' size={25} style={{ color: "#FFF", marginLeft: 13 }} onPress={() => props.navigation.navigate('Home')}></Icon> :
+                props.isHome === false ? <IonIcon name='arrow-back' size={25} style={{ color: "#FFF", marginLeft: 13 }} onPress={() => props.navigation}></IonIcon>
+                    : <IonIcon name='arrow-back' size={25} style={{ opacity: 0, marginLeft: 13 }}></IonIcon>}
+            <Text style={{ alignSelf: 'center', fontSize: 23, color: "#FFF", opacity: 1 }}>{props.titleName}</Text>
+            {props.onClose !== undefined ? <MCIcon name='close-circle' size={20} onPress={props.onClose} style={{ color: "#FFF" }} alignSelf={"flex-end"}></MCIcon> :
+                <Icon name="question-circle" size={25} style={{ color: "#FFF", marginRight: 13 }}></Icon>}
+        </View>
+    );
 }
 
 export { ActivityBar }
