dcc9439e93dfa5151e6ae136c46d3b585231c401
Giacomo Bruno <s301311@studenti.polito.it>
2023-01-08 18:27:01 +0100

    Implemented Tips logic

--- Components/Tips/Tips.js
+++ Components/Tips/Tips.js
@@ -1,16 +1,134 @@
+import React from "react";
 import { Button, Text } from "@rneui/themed";
-import { TouchableHighlight } from "react-native";
+import PaginationDot from 'react-native-animated-pagination-dot'
 import styles from "../../Globals/Styles";
+import {
+    StyleSheet,
+    View,
+    Animated,
+    FlatList,
+    useWindowDimensions,
+    TouchableHighlight
+} from 'react-native';
+import { TIPS_DATA } from "./TipsData";
+import { ActivityBar } from "../../Globals/Components";
 
-const Tips = ({navigation}) => {
+
+const Tips = ({ navigation }) => {
     return <>
-    <Text style={{color:'black', margin:20, alignSelf:'center', fontSize:30}}>
-        Tips page sample
-    </Text>
-    <TouchableHighlight style={{...styles.button, width:"40%", alignSelf:'center'}} onPress={() => navigation.navigate('Home')}>
-        <Text style={{color:'white', alignSelf:'center'}}>Home page</Text>
-    </TouchableHighlight>
+        <TipsItems navigation={navigation}/>
     </>
 }
+
+const TipsItems = (props) => {
+    const [curPage, setCurPage] = React.useState(0);
+    const { width } = useWindowDimensions();
+    const scrollX = React.useRef(new Animated.Value(0)).current;
+    const flatListRef = React.useRef(null);
+    const changeCurrentPage = React.useCallback(({ viewableItems, changed }) => {
+        if (viewableItems && viewableItems.length > 0) {
+            setCurPage(viewableItems[0].index);
+        }
+    }, []);
+    const renderItem = React.useCallback(
+        ({ item }) => {
+            return (
+                <View style={[otherStyles.itemContainer, { width: width - 80 }]}>
+                    <Text>{item.title}</Text>
+                    <Animated.Text>{item.description}</Animated.Text>
+                </View>
+            );
+        }, [width],
+    );
+
+    const keyExtractor = React.useCallback((item) => item.key, []);
+
+    return <>
+        <ActivityBar titleName="Tips" navigation={props.navigation}/>
+        <View style={{...otherStyles.container, marginTop:50}}>
+            <FlatList
+                flex={1}
+                ref={flatListRef}
+                data={TIPS_DATA}
+                keyExtractor={keyExtractor}
+                showsHorizontalScrollIndicator={false}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                    {
+                        useNativeDriver: false,
+                    },
+                )}
+                style={otherStyles.flatList}
+                pagingEnabled
+                horizontal
+                decelerationRate={'normal'}
+                scrollEventThrottle={16}
+                renderItem={renderItem}
+                onViewableItemsChanged={changeCurrentPage}
+            />
+        </View>
+        <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center', flexDirection: 'row' }}>
+            <TouchableHighlight style={{ ...otherStyles.button, width: "40%", alignSelf: 'center' }} onPress={() => {
+                flatListRef.current.scrollToIndex({ animated: true, index: (curPage > 0 ? (curPage - 1) : 0) });
+            }}>
+                <Text style={{ color: 'white', alignSelf: 'center' }}>Back</Text>
+            </TouchableHighlight>
+            <PaginationDot
+                activeDotColor={'black'}
+                curPage={curPage}
+                maxPage={4}
+            />
+            <TouchableHighlight style={{ ...otherStyles.button, width: "40%", alignSelf: 'center' }} onPress={() => {
+                flatListRef.current.scrollToIndex({ animated: true, index: (curPage < 3 ? (curPage + 1) : 3) });
+            }}>
+                <Text style={{ color: 'white', alignSelf: 'center' }}>Next</Text>
+            </TouchableHighlight>
+        </View>
+    </>;
+}
+
+const otherStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#e7e7e7',
+    },
+    text: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+    },
+    flatList: {
+        flex: 1,
+    },
+    dotContainer: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+    dotStyles: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        marginHorizontal: 3,
+    },
+    constainerStyles: {
+        top: 30,
+    },
+    itemContainer: {
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 40,
+        marginTop: 40,
+        marginHorizontal: 40,
+        borderRadius: 20,
+    },
+    button: {
+        borderColor: 'black',
+        borderWidth: 0,
+        borderRadius: 10,
+        backgroundColor: "#1D5C63",
+        padding: 10,
+        margin: 5,
+    }
+});
 
 export default Tips;
---
+++ Components/Tips/TipsData.js
@@ -0,0 +1,28 @@
+const TIPS_DATA = [
+    {
+        key: '1',
+        title: 'App showcase ✨',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        key: '2',
+        title: 'Introduction screen 🎉',
+        description:
+            "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. ",
+    },
+    {
+        key: '3',
+        title: 'And can be anything 🎈',
+        description:
+            'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. ',
+    },
+    {
+        key: '4',
+        title: 'And can be anything 2 🎈',
+        description:
+            'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. ',
+    }
+];
+
+export { TIPS_DATA }
