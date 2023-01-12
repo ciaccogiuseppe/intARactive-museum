afd75cb8255915f6841d757fea9ddc575db3855e
Giacomo Bruno <s301311@studenti.polito.it>
2023-01-12 18:12:27 +0100

    Added custom icons in achievements

--- Components/Achievements/Achievements.js
+++ Components/Achievements/Achievements.js
@@ -3,9 +3,13 @@ import { View } from 'react-native';
 import { Text, Icon } from "@rneui/themed";
 import { Overlay } from 'react-native-elements';
 import { ActivityBar } from '../../Globals/Components'
-import { achievementsList, getDone, getNeeded } from './AchievementLists';
+import { achievementsList, getDone, getNeeded, levels } from './AchievementLists';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useIsFocused } from '@react-navigation/native';
+import LV1 from '../../res/lv1';
+import LV2 from '../../res/lv2';
+import LV3 from '../../res/lv3';
+import LV4 from '../../res/lv4';
 
 const Achievements = (props) => {
     return <>
@@ -17,6 +21,21 @@ const Achievements = (props) => {
             <IconsList list={props.list} getDone={props.getDone} />
         </View>
     </>
+}
+
+const CustomIcon = (props) => {
+    switch (props.level) {
+        case levels.enjoyer:
+            return <LV1 {...props} fill={props.active ? 'black' : 'lightgrey'} />;
+        case levels.novice:
+            return <LV2 {...props} fill={props.active ? 'black' : 'lightgrey'} />;
+        case levels.fan:
+            return <LV3 {...props} fill={props.active ? 'black' : 'lightgrey'} />;
+        case levels.expert:
+            return <LV4 {...props} fill={props.active ? 'black' : 'lightgrey'} />;
+        default:
+            return <Icon name='trophy' type='font-awesome' size={50} color={props.active ? "black" : "lightgrey"}{...props} />;
+    }
 }
 
 function IconsList(props) {
@@ -31,7 +50,8 @@ function IconsList(props) {
     return <ScrollView ref={scrollViewRef}>
         <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: "wrap" }}>
             {props.list.map((achievement) => <AchievementIcon key={achievement.id} title={achievement.title} theme={achievement.theme}
-                done={props.getDone(achievement.theme)} needed={getNeeded(achievement)} date_obtained={achievement.date_obtained} />)}
+                done={props.getDone(achievement.theme)} needed={getNeeded(achievement)} date_obtained={achievement.date_obtained}
+                level={achievement.level} />)}
         </View>
     </ScrollView>;
 }
@@ -42,7 +62,7 @@ function AchievementIcon(props) {
         setVisible(visible => !visible);
     };
     return <View margin={5} flexDirection='column' alignItems='center' flexWrap='wrap'>
-        <Icon name='trophy' type='font-awesome' size={50} onPress={toggleOverlay} color={props.needed <= props.done ? "black" : "lightgrey"} />
+        <CustomIcon level={props.level} onPress={toggleOverlay} active={props.needed <= props.done} />
         <Text style={{ width: 75, color: 'black', alignSelf: 'center', margin: 5, textAlign: 'center', fontSize: 12 }}>{props.title}</Text>
         <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={{ backgroundColor: "#EDE6DB", color: "#EDE6DB", borderRadius: 15, width: '65%', height: '20%' }}>
             <View>
